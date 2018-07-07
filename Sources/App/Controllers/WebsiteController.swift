137aa5f9e2dd285d0784ebe4698337a820fc4f61
Guilherme Rambo <insidegui@gmail.com>
2018-07-07 19:36:48 -0300

    Showing username in card

--- Sources/App/Controllers/WebsiteController.swift
+++ Sources/App/Controllers/WebsiteController.swift
@@ -29,7 +29,14 @@ final class WebsiteController: RouteCollection {
         let query = Shortcut.query(on: req).range(0...20).sort(\.createdAt, .descending).all()
 
         return query.flatMap(to: View.self) { shortcuts in
-            return try req.view().render("index", ["shortcuts": shortcuts])
+            let userFutures = shortcuts.map({ $0.user.query(on: req).first() })
+
+            return userFutures.flatMap(to: View.self, on: req) { users in
+                let unwrappedUsers = users.compactMap({ $0 })
+                let cards = shortcuts.compactMap({ try? ShortcutCard($0, users: unwrappedUsers) })
+
+                return try req.view().render("index", ["cards": cards])
+            }
         }
     }
 
@@ -45,3 +52,20 @@ final class WebsiteController: RouteCollection {
     }
 
 }
+
+struct ShortcutCard: Codable {
+    let shortcut: Shortcut
+    let creator: User
+
+    init(_ shortcut: Shortcut, users: [User]) throws {
+        self.shortcut = shortcut
+
+        guard let user = users.first(where: { $0.id == shortcut.userID }) else {
+            throw Abort(.notFound)
+        }
+
+        self.creator = user
+    }
+}
+
+extension ShortcutCard: Content { }
