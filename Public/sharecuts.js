f9cdf03fff62d9ffc1cf4f1f46e3f9bf8d226c23
Guilherme Rambo <insidegui@gmail.com>
2018-07-07 19:40:53 -0300

    Reverted to link for download

--- Public/sharecuts.js
+++ Public/sharecuts.js
@@ -1,15 +1,3 @@
-function sharecuts() {
-    function download(shortcut) {
-        window.location.href = "/download/" + shortcut;
-    }
-
-    document.querySelectorAll("div.shortcut-card").forEach(function(card){
-        card.addEventListener("click", function(e) {
-            var shortcut = e.target.dataset.shortcut;
-            if (shortcut == undefined) return;
-            download(shortcut);
-        }, false);
-    });
-}
+function sharecuts() { }
 
 window.addEventListener("DOMContentLoaded", sharecuts, false);
