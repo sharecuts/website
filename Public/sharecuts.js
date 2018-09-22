6abb46ddc07fc1fd3586d3a8647327da6a4a98d7
Guilherme Rambo <insidegui@gmail.com>
2018-09-22 09:43:34 -0300

    Preventing deep link from opening on non-iOS devices

--- Public/sharecuts.js
+++ Public/sharecuts.js
@@ -1,3 +1,23 @@
-function sharecuts() { }
+function Sharecuts() {
 
-window.addEventListener("DOMContentLoaded", sharecuts, false);
+    const self = this;
+
+    this.canOpenShortcutsDeepLinks = function() {
+        return /iPhone|iPad|iPod/.test(navigator.platform);
+    }
+    
+    this.install = function(e) {
+        document.querySelector("a.card-shortcut").addEventListener("click", function(e){
+            if (self.canOpenShortcutsDeepLinks()) return;
+            
+            e.preventDefault();
+
+            alert("To download Shortcuts, open sharecuts.app on an iOS device with the Shortcuts app installed.");
+        }, false);
+    }
+
+}
+
+const SC = new Sharecuts();
+
+window.addEventListener("DOMContentLoaded", SC.install, false);
